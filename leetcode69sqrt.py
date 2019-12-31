00aee28a6aca13b9702c2fee45d4c681798f314a
Yi Wang <52504365+IanWang15@users.noreply.github.com>
2019-12-30 23:28:25 -0600

    leetcode 69
    sqrt

---
+++ leetcode69sqrt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 23:12:32 2019
+
+@author: yiwang
+"""
+
+class Solution():
+    def findsqrt(self, x):
+        start = 0
+        end = x
+        while (start + 1 < end):
+            mid = (end - start) // 2 + start
+            if mid**2 < x:
+                start = mid
+            elif mid**2 == x:
+                start = mid
+            else:
+                end = mid
+        if end**2 < x:
+            return end
+        else:
+            return start
+                
+        
+t = Solution()
+t0 = t.findsqrt(9)
+print(t0)
+
+
+# Exponentiation is **, not ^
