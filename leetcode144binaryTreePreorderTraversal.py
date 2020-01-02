3b68c49456339570dac6b6e2867ac24523e7dd32
Yi Wang <52504365+IanWang15@users.noreply.github.com>
2020-01-01 23:55:23 -0600

    leetcode 144
    Binary tree preorder using traversal method

---
+++ leetcode144binaryTreePreorderTraversal.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 15:29:05 2020
+
+@author: yiwang
+"""
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode):
+        self.results = []
+        self.traverse(root)
+        return self.results
+    
+    def traverse(self, node):
+        if node is None:
+            return
+        self.results.append(node.val)
+        self.traverse(node.left)
+        self.traverse(node.right)
+
+        
+        
+#[1,None,2,3]
+t = TreeNode(1)
+t.left = TreeNode(2)
+t.right = TreeNode(3)
+t.left.left = TreeNode(4)
+t.left.right = TreeNode(5)
+s = Solution()
+t0 = s.preorderTraversal(t)
+print(t0)
