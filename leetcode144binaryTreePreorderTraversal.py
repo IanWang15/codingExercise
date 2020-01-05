7b82964bc817fb991149bcc55680afd769475cab
Yi Wang <52504365+IanWang15@users.noreply.github.com>
2020-01-05 12:13:54 -0600

    Update leetcode144binaryTreePreorderTraversal.py
    add another method to use global variable

--- leetcode144binaryTreePreorderTraversal.py
+++ leetcode144binaryTreePreorderTraversal.py
@@ -6,7 +6,6 @@ Created on Wed Jan  1 15:29:05 2020
 @author: yiwang
 """
 # traverse method
-
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -20,13 +19,26 @@ class Solution:
         return self.results
     
     def traverse(self, node):
-        if node is None:
+        if not node:
             return
         self.results.append(node.val)
         self.traverse(node.left)
         self.traverse(node.right)
-
         
+# it is equal to the follow method
+# it is the same, both are using global variable
+       
+#    def preorderTraversal(self, root: TreeNode):
+#        results = []
+#        self.traverse(root, results)
+#        return results
+    
+#    def traverse(self, node, results):
+#        if not node:
+#            return
+#        results.append(node.val)
+#        self.traverse(node.left, results)
+#        self.traverse(node.right, results)
         
 #[1,None,2,3]
 t = TreeNode(1)
